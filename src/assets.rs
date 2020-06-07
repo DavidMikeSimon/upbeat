a75c281a63463a7125e00d1b0a16c889b6d0246c
David Simon <dsimon@newrelic.com>
2020-06-07 13:00:09 -0700

    Factor out now_line

--- src/assets.rs
+++ src/assets.rs
@@ -1,6 +1,11 @@
 use ggez::{graphics, Context};
 
 pub struct Assets {
+  pub music_bar_height: f32,
+
+  pub now_line_width: f32,
+  pub now_line: graphics::Mesh,
+
   pub arrow_width: f32,
   pub up_arrow: graphics::Mesh,
   pub down_arrow: graphics::Mesh,
@@ -8,6 +13,21 @@ pub struct Assets {
 
 impl Assets {
   pub fn new(ctx: &mut Context) -> Assets {
+    let music_bar_height = 400.0;
+
+    let now_line_width = 2.0;
+
+    let now_line = graphics::Mesh::new_line(
+      ctx,
+      &[
+        nalgebra::Point2::new(0.0, 0.0),
+        nalgebra::Point2::new(0.0, music_bar_height)
+      ],
+      now_line_width,
+      graphics::BLACK
+    ).unwrap();
+
+
     let arrow_width = 20.0;
     let arrow_height = 10.0;
 
@@ -34,6 +54,11 @@ impl Assets {
     ).unwrap();
 
     Assets {
+      music_bar_height: music_bar_height,
+
+      now_line_width: now_line_width,
+      now_line: now_line,
+
       arrow_width: arrow_width,
       up_arrow: up_arrow,
       down_arrow: down_arrow,
