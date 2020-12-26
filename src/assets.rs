c99b296e83e7e57936deb6ed1a66a45977f878d4
David Simon <dsimon@newrelic.com>
2020-12-26 14:05:27 -0800

    Revert "Trying to have notes merge from both sides into a center line, but it doesnt feel right"
    This reverts commit 1b8a3ca50420362639b4a15b4743fabc699e5465.

--- src/assets.rs
+++ src/assets.rs
@@ -19,9 +19,13 @@ pub struct Assets {
   pub music_bar_height: f32,
   pub music_bar: graphics::Mesh,
 
+  pub command_window_width: f32,
+  pub command_window: graphics::Mesh,
+
   pub command_cursor: graphics::Mesh,
 
   pub now_line_width: f32,
+  pub now_line_x_offset: f32,
   pub now_line: graphics::Mesh,
 
   pub arrow_width: f32,
@@ -64,15 +68,24 @@ impl Assets {
     let window = graphics::screen_coordinates(ctx);
 
     let music_bar_height = 200.0;
+    let command_window_width = 300.0;
 
     let music_bar = graphics::Mesh::new_rectangle(
       ctx,
       graphics::DrawMode::fill(),
-      graphics::Rect::new(0.0, 0.0, window.w, music_bar_height),
+      graphics::Rect::new(0.0, 0.0, window.w - command_window_width, music_bar_height),
       graphics::Color::from_rgba(210, 210, 210, 128)
     ).unwrap();
 
+    let command_window = graphics::Mesh::new_rectangle(
+      ctx,
+      graphics::DrawMode::fill(),
+      graphics::Rect::new(0.0, 0.0, command_window_width, music_bar_height),
+      graphics::Color::from_rgba(192, 192, 192, 128)
+    ).unwrap();
+
     let now_line_width = 2.0;
+    let now_line_x_offset = 100.0;
 
     let now_line = graphics::Mesh::new_line(
       ctx,
@@ -137,9 +150,13 @@ impl Assets {
       music_bar_height: music_bar_height,
       music_bar: music_bar,
 
+      command_window_width: command_window_width,
+      command_window: command_window,
+
       command_cursor: command_cursor,
 
       now_line_width: now_line_width,
+      now_line_x_offset: now_line_x_offset,
       now_line: now_line,
 
       arrow_width: arrow_width,
